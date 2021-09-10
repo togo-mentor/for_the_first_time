0b7e9b47a4633ddd143dd9fb95ba10f3ce6667b4
togo-mentor <uint2435@gmail.com>
2021-09-11 07:44:20 +0900

    ページ分割

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './tab_page.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -31,28 +32,6 @@ class _MainPageState extends State<MainPage> {
               TabPage(title: "ダッシュボード", icon: Icons.dashboard),
             ]
         ),
-      ),
-    );
-  }
-}
-
-class TabPage extends StatelessWidget {
-
-  final IconData icon;
-  final String title;
-
-  const TabPage({key, required this.icon, required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final TextStyle? textStyle = Theme.of(context).textTheme.headline4;  // 文字のスタイル
-    return Center(
-      child:Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Icon(icon, size: 72.0, color: textStyle!.color),
-          Text(title, style: textStyle),
-        ],
       ),
     );
   }
