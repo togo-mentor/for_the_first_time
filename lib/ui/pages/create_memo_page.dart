eef61f76e65b2224126bdda3544eecf6766abaf5
togo-mentor <uint2435@gmail.com>
2021-09-11 07:56:30 +0900

    とりあえずフォーム表示した

--- lib/ui/pages/create_memo_page.dart
+++ lib/ui/pages/create_memo_page.dart
@@ -9,7 +9,26 @@ class _CreateMemoPageState extends State<CreateMemoPage> {
 
   Widget build(BuildContext context) {
     return Center(
-
+      child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: '名前',
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: '趣味',
+              ),
+            ),
+            ElevatedButton(
+              child: Text('新規登録'),
+              onPressed: () {
+                // TODO: 新規登録
+              },
+            ),
+          ],
+        ),
     );
   }
 }
