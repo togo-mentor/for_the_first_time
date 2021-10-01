1170df3a91883c0ed37f4149b4acdc3260a25f9e
togo-mentor <uint2435@gmail.com>
2021-10-02 08:26:12 +0900

    reactive formへ切り替え

--- lib/ui/pages/create_memo_page.dart
+++ lib/ui/pages/create_memo_page.dart
@@ -4,6 +4,7 @@ import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:flutter/material.dart';
 import 'package:amplify_flutter/amplify.dart';
 import 'package:for_the_first_time/models/genre.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 import '../../models/post.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,12 +14,19 @@ class CreateMemoPage extends StatefulWidget {
 }
 
 class _CreateMemoPageState extends State<CreateMemoPage> {
-  
-  // フォーム送信後にフォームの入力内容をリセットするためのkey
-  final GlobalKey<FormState> _formKeyScreen1 = GlobalKey<FormState>();
+  //  final fieldText = TextEditingController();
   String content = '';
   Genre genre = Genre(0, '');
-  bool _selected = false;
+    FormGroup form = FormGroup({
+    'content': FormControl<String>(
+      validators: [
+        Validators.required,
+      ],
+    ),
+    'genreId': FormControl<Genre>(validators: [
+      Validators.required,
+    ]),
+  });
 
   @override
   void initState() {
@@ -38,10 +46,11 @@ class _CreateMemoPageState extends State<CreateMemoPage> {
         headers: {"Content-Type": "application/json"}
       );
       if (response.statusCode == 200) {
-        _formKeyScreen1.currentState?.reset(); // フォームの入力内容をリセット
-        // stateをリセットする
-        content = '';
-        _selected = false;
+        form.control('content').value = '';
+        form.control('genreId').value = null;
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('保存に成功しました！'),
+        ));
       }
     } catch (error) {
       print(error);
@@ -64,65 +73,51 @@ class _CreateMemoPageState extends State<CreateMemoPage> {
             SizedBox(
               height: 30,
             ),
-            Form (
-              key: _formKeyScreen1,
-              child: Column (
-                children: <Widget>[
-                  TextFormField (
-                    decoration: InputDecoration(
-                      hintText: '今日あった「人生初」の出来事を記録してください',
-                      labelText: "出来事",
-                      border: OutlineInputBorder(),
-                      floatingLabelBehavior:FloatingLabelBehavior.always, // labelを上部に固定
-                    ),
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'このフィールドは必須です';
-                      }
-                    },
-                    autofocus: true,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: 10,
-                    onChanged: (value) {
-                      content = value;
+            ReactiveForm(
+            formGroup: form,
+            child: Column(
+              children: <Widget>[
+                ReactiveTextField(
+                  formControlName: 'content',
+                  maxLines: 10,
+                  autofocus: true,
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    hintText: '今日あった「人生初」の出来事を記録してください',
+                    labelText: "出来事",
+                    border: OutlineInputBorder(),
+                    floatingLabelBehavior:FloatingLabelBehavior.always, // labelを上部に固定
+                  ),
+                  validationMessages: (control) => {
+                      ValidationMessage.required: '出来事を入力してください。',
                     },
                   ),
                   SizedBox(
 				            height: 20,
                   ),
-                  DropdownButtonFormField<Genre>(
-                  value: _selected ? genre : null,
-                  iconSize: 24,
-                  elevation: 16,
-                  onChanged: (Genre? newValue) {
-                    setState(() {
-                      genre = newValue!;
-                    });
-                  },
-                  validator: (Genre? value) {
-                      if (value == null) {
-                        return 'このフィールドは必須です';
-                      }
-                    },
-                   decoration: InputDecoration(
-                    labelText: 'ジャンルを選択',
-                    border: OutlineInputBorder(),
-                  ),
-                  items: genreList
-                        .map<DropdownMenuItem<Genre>>((Genre item) {
-                      return DropdownMenuItem<Genre>(
-                        value: item,
-                        child: Text(item.name),
-                      );
-                    }).toList(),
+                  ReactiveDropdownField(
+                    iconSize: 24,
+                    elevation: 16,
+                    formControlName: 'genreId',
+                    decoration: InputDecoration(
+                      labelText: 'ジャンルを選択',
+                      border: OutlineInputBorder(),
+                    ),
+                    items: genreList
+                          .map<DropdownMenuItem<Genre>>((Genre item) {
+                        return DropdownMenuItem<Genre>(
+                          value: item,
+                          child: Text(item.name),
+                        );
+                      }).toList(),
                   ),
                   SizedBox(
-				            height: 25,
+                    height: 25,
                   ),
                   ElevatedButton(
                     child: Text('保存する'),
                     onPressed: () {
-                      _createPost(content, genre.id);
+                      form.valid ? _createPost(form.control('content').value, form.control('genreId').value.id) : null;
                     },
                     style: ButtonStyle(
                       backgroundColor: MaterialStateProperty.all(Colors.blue),
@@ -130,8 +125,8 @@ class _CreateMemoPageState extends State<CreateMemoPage> {
                     ),
                   ),
                 ]
-              )
-            ),
+              ),
+            )
           ],
         ),
     );
