8e2217f04e8613b7828d2a13905ec7788328228b
togo-mentor <uint2435@gmail.com>
2021-10-03 11:54:29 +0900

    ReactiveForm

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:for_the_first_time/ui/pages/signup_page.dart';
-import '../../service/auth_credentials.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 import 'main_page.dart';
 
 class LoginPage extends StatefulWidget {
@@ -18,6 +18,18 @@ class _LoginPageState extends State<LoginPage> {
   final FirebaseAuth auth = FirebaseAuth.instance;
   UserCredential? _result;
   User? _user;
+  FormGroup form = FormGroup({
+    'password': FormControl<String>(
+      validators: [
+        Validators.required,
+      ],
+    ),
+    'email': FormControl<String>(
+      validators: [
+        Validators.required,
+      ],
+    )
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -26,116 +38,115 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                // メールアドレスの入力フォーム
-                Padding(
-                  padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
-                  child:TextFormField(
-                    decoration: InputDecoration(
-                      labelText: "メールアドレス"
-                    ),
-                    onChanged: (String value) {
-                      _login_Email = value;
-                    },
-                  )
-                ),
-
-                // パスワードの入力フォーム
-                Padding(
-                  padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 10.0),
-                  child:TextFormField(
-                    decoration: InputDecoration(
-                      labelText: "パスワード（8～20文字）"
-                    ),
-                    obscureText: true,  // パスワードが見えないようRにする
-                    maxLength: 20,  // 入力可能な文字数
-                    onChanged: (String value) {
-                      _login_Password= value;
-                    },
-                  ),
-                ),
-
-                // ログイン失敗時のエラーメッセージ
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20.0, 0, 20.0, 5.0),
-                  child:Text(_infoText,
-                    style: TextStyle(color: Colors.red),),
-                ),
-                ButtonTheme(
-                  minWidth: 350.0,
-                  child: SizedBox(
-                    width: 300,
-                    child: 
-                      ElevatedButton(
-                        onPressed: () async {
-                          try {
-                            // メール/パスワードでユーザー登録
-                            _result = await auth.signInWithEmailAndPassword(
-                              email: _login_Email,
-                              password: _login_Password,
-                            );
-
-                            // ログイン成功
-                            // ログインユーザーのIDを取得
-                            _user = _result!.user;
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => MainPage(),
-                              )
-                            );
-                            
-                          } catch (e) {
-                            // ログインに失敗した場合
-                            setState(() {
-                              print(e);
-                            });
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.blue,
+                ReactiveForm(
+                  formGroup: form,
+                  child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 10.0),
+                      child: ReactiveTextField(
+                        formControlName: 'email',
+                        decoration: InputDecoration(
+                          labelText: "メールアドレス"
                         ),
-                        // ボタン内の文字や書式
-                        child: Text('ログイン',
-                          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
                       ),
                     ),
-                  )
-              )],
-            ),
-        // 画面下にボタンの配置
-        ),
-        bottomNavigationBar: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child:ButtonTheme(
-                minWidth: 350.0,
-                // height: 100.0,
-                child: ElevatedButton(
-                  child: Text('アカウントを作成する',
-                    style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blue[50],
-                  ),
-
-                  // ボタンクリック後にアカウント作成用の画面の遷移する。
-                  onPressed: (){
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        fullscreenDialog: true,
-                        builder: (BuildContext context) => SignUpPage(),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 10.0),
+                      child: ReactiveTextField(
+                        formControlName: 'password',
+                        decoration: InputDecoration(
+                          labelText: "パスワード（8～20文字）"
+                        ),
+                        obscureText: true,  // パスワードが見えないようRにする
+                        maxLength: 20,  // 入力可能な文字数
                       ),
-                    );
-                  }
+                    ),                  
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ButtonTheme(
+                      minWidth: 350.0,
+                      child: SizedBox(
+                      width: 300,
+                      child: 
+                        ElevatedButton(
+                          onPressed: () async {
+                            try {
+                              // メール/パスワードでユーザー登録
+                              _result = await auth.signInWithEmailAndPassword(
+                                email: _login_Email,
+                                password: _login_Password,
+                              );
 
+                              // ログイン成功
+                              // ログインユーザーのIDを取得
+                              _user = _result!.user;
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => MainPage(),
+                                )
+                              );
+                              
+                            } catch (e) {
+                              // ログインに失敗した場合
+                              setState(() {
+                                print(e);
+                              });
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                          ),
+                          // ボタン内の文字や書式
+                          child: Text('ログイン',
+                            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+                          ),
+                    ),
+                  )
+                )
+              ]
+            ),
+            )
+          ]
+        ), // 画面下にボタンの配置
+      ),
+      bottomNavigationBar: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child:ButtonTheme(
+                  minWidth: 350.0,
+                  // height: 100.0,
+                  child: ElevatedButton(
+                    child: Text('アカウントを作成する',
+                      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue[50],
+                    ),
+
+                    // ボタンクリック後にアカウント作成用の画面の遷移する。
+                    onPressed: (){
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          fullscreenDialog: true,
+                          builder: (BuildContext context) => SignUpPage(),
+                        ),
+                      );
+                    }
+
+                  ),
                 ),
               ),
-            ),
-          ]
-        )
+            ]
+          )
     );
   }
 }
