e6f173b2bbf0c870bd27bcf7959e98de12e2b165
togo-mentor <uint2435@gmail.com>
2021-10-08 08:15:44 +0900

    メールの再送信メソッド定義

--- lib/ui/pages/verification_page.dart
+++ lib/ui/pages/verification_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:for_the_first_time/models/auth.dart';
+import 'package:provider/provider.dart';
 
 class VerificationPage extends StatefulWidget {
   @override
@@ -42,7 +45,7 @@ class _VerificationPageState extends State<VerificationPage> {
             minWidth: 200.0,  
             child: ElevatedButton(
               onPressed: () async {
-                
+                await _resendVerificationEmail(context);
               },
               // ボタン内の文字や書式
               child: Text('確認メールを再送信',
@@ -56,5 +59,14 @@ class _VerificationPageState extends State<VerificationPage> {
         ],
       )
     );
+  }
+
+  Future<bool> _resendVerificationEmail(BuildContext context) async {
+    bool loggedIn = false;
+    EasyLoading.show(status: 'loading...');
+    // print(context.read<Auth>().user);
+    await context.read<Auth>().user!.sendEmailVerification();
+    EasyLoading.dismiss();
+    return loggedIn;
   }
 }
