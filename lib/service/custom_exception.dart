4c3372104f5f55c8915f7d0ace8b6d437063fe5f
togo-mentor <uint2435@gmail.com>
2021-10-17 06:46:12 +0900

    ovveride

--- lib/service/custom_exception.dart
+++ lib/service/custom_exception.dart
@@ -1,11 +1,12 @@
 class CustomException implements Exception {
-  final _message;
-  final _prefix;
+  final String? _message;
+  final String? _prefix;
 
   CustomException([this._message, this._prefix]);
 
+  @override
   String toString() {
-  return "$_prefix$_message";
+    return "$_prefix$_message";
   }
 }
 
