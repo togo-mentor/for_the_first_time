326fa0feb355192699b93334523c46bdc906a937
togo-mentor <uint2435@gmail.com>
2021-10-21 07:33:48 +0900

    とりあえずグラフ表示した

--- lib/ui/pages/dashboard_page.dart
+++ lib/ui/pages/dashboard_page.dart
@@ -1,14 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:for_the_first_time/service/post_service.dart';
+import 'package:for_the_first_time/ui/components/indicator.dart';
 import '../../models/post.dart';
-
-/// Simple pie chart example.
-// EXCLUDE_FROM_GALLERY_DOCS_START
-import 'dart:math';
-// EXCLUDE_FROM_GALLERY_DOCS_END
-import 'package:charts_flutter/flutter.dart' as charts;
-import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class DashBoardPage extends StatefulWidget {
   @override
@@ -17,15 +12,13 @@ class DashBoardPage extends StatefulWidget {
 
 class _DashBoardPageState extends State<DashBoardPage> {
   List<Post> _posts = [];
-  final ScrollController _controller = ScrollController();
   final bool animate = false;
-  final List<charts.Series<dynamic, num>> seriesList = [];
+   int touchedIndex = -1;
 
   @override
   void initState() {
     super.initState();
     _initializeApp();
-    createChartData();
   }
 
   Future<void> _initializeApp() async {
@@ -44,22 +37,168 @@ class _DashBoardPageState extends State<DashBoardPage> {
   }
 
   // 取得したpostデータからグラフデータを生成する
-  void createChartData() {
-
+  Widget createChartData() {
+    return  Column(
+      children: <Widget>[
+        Expanded(
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: PieChart(
+              PieChartData(
+                  pieTouchData:
+                      PieTouchData(touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                    setState(() {
+                      if (!event.isInterestedForInteractions ||
+                          pieTouchResponse == null ||
+                          pieTouchResponse.touchedSection == null) {
+                        touchedIndex = -1;
+                        return;
+                      }
+                      touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
+                    });
+                  }),
+                  borderData: FlBorderData(
+                    show: false,
+                  ),
+                  sectionsSpace: 0,
+                  centerSpaceRadius: 10,
+                  sections: showingSections(),
+              ),
+            ),
+          )
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Indicator(
+              color: const Color(0xff0293ee),
+              text: 'One',
+              isSquare: false,
+              size: touchedIndex == 0 ? 18 : 16,
+              textColor: touchedIndex == 0 ? Colors.black : Colors.grey,
+            ),
+            Indicator(
+              color: const Color(0xfff8b250),
+              text: 'Two',
+              isSquare: false,
+              size: touchedIndex == 1 ? 18 : 16,
+              textColor: touchedIndex == 1 ? Colors.black : Colors.grey,
+            ),
+            Indicator(
+              color: const Color(0xff845bef),
+              text: 'Three',
+              isSquare: false,
+              size: touchedIndex == 2 ? 18 : 16,
+              textColor: touchedIndex == 2 ? Colors.black : Colors.grey,
+            ),
+            Indicator(
+              color: const Color(0xff13d38e),
+              text: 'Four',
+              isSquare: false,
+              size: touchedIndex == 3 ? 18 : 16,
+              textColor: touchedIndex == 3 ? Colors.black : Colors.grey,
+            ),
+          ],
+        )
+      ]
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return _posts.isNotEmpty
-        ? Text('test')
+        ? createChartData()
       : Center(child: CircularProgressIndicator());
   }
-}
 
-/// Sample linear data type.
-class LinearSales {
-  final int year;
-  final int sales;
+  List<PieChartSectionData> showingSections() {
+    Color darken(Color color, [double amount = .1]) {
+      assert(amount >= 0 && amount <= 1);
 
-  LinearSales(this.year, this.sales);
+      final hsl = HSLColor.fromColor(color);
+      final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+
+      return hslDark.toColor();
+    }
+
+
+    return List.generate(
+      4,
+      (i) {
+        final isTouched = i == touchedIndex;
+        final opacity = isTouched ? 1.0 : 0.6;
+
+        const color0 = Color(0xff0293ee);
+        const color1 = Color(0xfff8b250);
+        const color2 = Color(0xff845bef);
+        const color3 = Color(0xff13d38e);
+
+        switch (i) {
+          case 0:
+            return PieChartSectionData(
+              color: color0.withOpacity(opacity),
+              value: 25,
+              title: '',
+              radius: 80,
+              titleStyle: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff044d7c)),
+              titlePositionPercentageOffset: 0.55,
+              borderSide: isTouched
+                  ? BorderSide(color: darken(color0, 40), width: 6)
+                  : BorderSide(color: color0.withOpacity(0)),
+            );
+          case 1:
+            return PieChartSectionData(
+              color: color1.withOpacity(opacity),
+              value: 25,
+              title: '',
+              radius: 65,
+              titleStyle: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff90672d)),
+              titlePositionPercentageOffset: 0.55,
+              borderSide: isTouched
+                  ? BorderSide(color: darken(color1, 40), width: 6)
+                  : BorderSide(color: color2.withOpacity(0)),
+            );
+          case 2:
+            return PieChartSectionData(
+              color: color2.withOpacity(opacity),
+              value: 25,
+              title: '',
+              radius: 60,
+              titleStyle: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff4c3788)),
+              titlePositionPercentageOffset: 0.6,
+              borderSide: isTouched
+                  ? BorderSide(color: darken(color2, 40), width: 6)
+                  : BorderSide(color: color2.withOpacity(0)),
+            );
+          case 3:
+            return PieChartSectionData(
+              color: color3.withOpacity(opacity),
+              value: 25,
+              title: '',
+              radius: 70,
+              titleStyle: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff0c7f55)),
+              titlePositionPercentageOffset: 0.55,
+              borderSide: isTouched
+                  ? BorderSide(color: darken(color3, 40), width: 6)
+                  : BorderSide(color: color2.withOpacity(0)),
+            );
+          default:
+            throw Error();
+        }
+      },
+    );
+  }
 }
