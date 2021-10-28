f15ef68d1b3ee40c6615c5704e9cd2ddbeb01a46
togo-mentor <uint2435@gmail.com>
2021-10-29 07:48:19 +0900

    print削除

--- lib/ui/components/genre_pie_chart.dart
+++ lib/ui/components/genre_pie_chart.dart
@@ -43,7 +43,6 @@ class _GenrePieChartState extends State<GenrePieChart> {
                     touchedIndex = -1;
                     return;
                   }
-                  print(pieTouchResponse.touchedSection!.touchedSectionIndex);
                   touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
                 });
               }),
